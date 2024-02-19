3287c7c6bbb4d46f682c11e1d7b456b4f3c5eaf1
pedrokpp <pedrokp@id.uff.br>
2024-02-19 11:14:16 -0300

    removed one dark theme

--- init.lua
+++ init.lua
@@ -201,20 +201,6 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require('onedark').setup {
-        -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-      }
-      require('onedark').load()
-    end,
-  },
-
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
